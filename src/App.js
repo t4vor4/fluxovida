e51f31c183284ab15ec95620b0c69b6202b4b549
Fernando <fernando@agenciaeplus.com.br>
2020-03-08 23:48:52 -0300

    [update] página montada com botão de reiniciar. Agora precissa estilizar bonito.

--- src/App.js
+++ src/App.js
@@ -17,11 +17,6 @@ function App() {
     JSON.parse($f.init($f.idade()))
   );
 
-  const s1 = initial.section1;
-  const s2 = initial.section2;
-  const s3 = initial.section3;
-  const s4 = initial.section4;
-
 
   // useEffect(() => {
   //   console.log(s4);
@@ -29,23 +24,28 @@ function App() {
   //   // console.log(initial.section3)
   //   // console.log(initial.section4)
   // })
+
   return (
     <div className="content_page">
 
       <header>
-        <h1>Fluxovida <small>um gerador de background para personagens cyberpunks</small></h1>
+        <h1>Fluxo<br/>vida <small>um gerador de background para personagens cyberpunks</small></h1>
       </header>
-      <Origem content={s1}/>
-      <Antecedente content={s2} />
-      <Motivacoes content={s3} />
-      <Eventos content={s4} />
-
+      
+      <Origem content={initial.section1}/>
+      <Antecedente content={initial.section2} />
+      <Motivacoes content={initial.section3} />
+      <Eventos content={initial.section4} />
+      
+      <aside>
+        <button onClick={ _ => setInitial(JSON.parse($f.init($f.idade())))}>Gerar nova história</button>
+      </aside>
       
       <footer></footer>
       {/* {initial} */}
 
 
-      {/* <button onClick={ _ => setInitial($f.init($f.idade()))}>Re-rolar</button> */}
+      
     </div>
   );
 }
