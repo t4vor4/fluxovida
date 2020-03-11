106f96a26fb22bf1bdff92b82552bb2eacdbb8a0
Fernando <fernando@agenciaeplus.com.br>
2020-03-10 23:50:47 -0300

    Updates

---
+++ precache-manifest.16197ecce6825d6d8805d9ba966c3cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cb81dd51316813d9453ec342db2aca",
+    "url": "/fluxovida/index.html"
+  },
+  {
+    "revision": "664450dff5ddc145c979",
+    "url": "/fluxovida/static/css/main.28b9554a.chunk.css"
+  },
+  {
+    "revision": "e1eac674a3e9fbe1121c",
+    "url": "/fluxovida/static/js/2.2d394fd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fluxovida/static/js/2.2d394fd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "664450dff5ddc145c979",
+    "url": "/fluxovida/static/js/main.651c4dd3.chunk.js"
+  },
+  {
+    "revision": "082b85b9e2651beccbd7",
+    "url": "/fluxovida/static/js/runtime-main.5e6ac8f1.js"
+  }
+]);
